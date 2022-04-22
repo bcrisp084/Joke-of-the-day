a8879700df16d6e56e92605cafdf0d17b7fa59b0
Brian Crisp <bcrisp084@yahoo.com>
2022-04-21 21:27:55 -0400

    retrieved response from api

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 let key = '16ad913096msh3132f772dc507f0p1eedddjsn83b05885f4cf';
-
+const setup = document.querySelector('.random-setup')
+const punchline = document.querySelector('.random-punchline')
 const settings = {
     "async": true,
     "crossDomain": true,
@@ -12,5 +13,9 @@ const settings = {
 };
 
 $.ajax(settings).done(function (response) {
-    console.log(response);
+    console.log(response.body[0].setup);
+    console.log(response.body[0].punchline)
+    punchline.textContent = response.body[0].setup
+    setup.textContent = response.body[0].punchline
 });
+

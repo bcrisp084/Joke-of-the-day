37fcc7d4c57b3632ca4e4acb9de1bee88c809252
Brian Crisp <bcrisp084@yahoo.com>
2022-04-27 21:11:12 -0400

    file change

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,23 @@
-let key = "16ad913096msh3132f772dc507f0p1eedddjsn83b05885f4cf";
-const setup = document.querySelector(".random-setup");
-const punchline = document.querySelector(".random-punchline");
-const settings = {
-  async: true,
-  crossDomain: true,
-  url: "https://dad-jokes.p.rapidapi.com/random/joke",
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
-    "X-RapidAPI-Key": "16ad913096msh3132f772dc507f0p1eedddjsn83b05885f4cf",
-  },
-};
+// let key = "16ad913096msh3132f772dc507f0p1eedddjsn83b05885f4cf";
+// const setup = document.querySelector(".random-setup");
+// const punchline = document.querySelector(".random-punchline");
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://dad-jokes.p.rapidapi.com/random/joke",
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+//     "X-RapidAPI-Key": "16ad913096msh3132f772dc507f0p1eedddjsn83b05885f4cf",
+//   },
+// };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-  console.log(response.body[0].setup);
-  console.log(response.body[0].punchline);
-  setup.textContent = response.body[0].setup;
-  punchline.textContent = response.body[0].punchline;
-});
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+//   console.log(response.body[0].setup);
+//   console.log(response.body[0].punchline);
+//   setup.textContent = response.body[0].setup;
+//   punchline.textContent = response.body[0].punchline;
+// });
+
+console.log(this);
